af56cb7c0d1ce554775423572b8b34093404c9bb
Sebastián Tamashiro <sebastian@alotama.com>
2021-05-05 20:07:13 -0300

    fix build

---
+++ pages/contacto/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '../../components/button'
+
+const ContactPage = () => {
+  return (
+    <section>
+      <article>
+        <div>
+          <h1>Proyectos.</h1>
+          <h2>Desarrollo Front-End</h2>
+        </div>
+        <Button>Ver todos</Button>
+      </article>
+    </section>
+  )
+}
+
+export default ContactPage
