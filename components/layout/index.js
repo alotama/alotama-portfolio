4d43792425392e5972e49ecbbfba522c119df62b
Sebastián Tamashiro <sebastian@alotama.com>
2021-05-07 20:06:43 -0300

    general layout

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Header from '../header'
 import Footer from '../footer'
 import styles from '../../styles/components/layout.module.scss'
@@ -5,6 +6,10 @@ import styles from '../../styles/components/layout.module.scss'
 const Layout = ({children}) => {
   return (
     <>
+      <Head>
+        <title>alotama - Sebastián Tamashiro</title>
+        <meta name="robots" content="noindex" />
+      </Head>
       <Header />
       <main className={styles.master_container}>
         {children}
--- pages/contacto/index.js
+++ pages/contacto/index.js
@@ -1,17 +1,52 @@
 import React from 'react'
+import Layout from '../../components/layout'
 import Button from '../../components/button'
+import styles from '../../styles/pages/contact.module.scss'
 
 const ContactPage = () => {
   return (
-    <section>
-      <article>
-        <div>
-          <h1>Proyectos.</h1>
-          <h2>Desarrollo Front-End</h2>
+    <Layout>
+      <article className={styles.heroContact}>
+        <div className={styles.heroContact_container}>
+          <h1 className={styles.heroContact_title}>Contacto</h1>
+          <h2 className={styles.heroContact_subtitle}>Desarrollo Front-End</h2>
         </div>
-        <Button>Ver todos</Button>
+        <section className={styles.contactSection}>
+          <form className={styles.contactForm}>
+            <div className={styles.contactName}>
+              <label className={styles.contact_label}>Nombre</label>
+              <input className={styles.contact_input} type="text" />
+            </div>
+            <div className={styles.contactEmail}>
+              <label className={styles.contact_label}>E-mail</label>
+              <input className={styles.contact_input} type="text" />
+            </div>
+            <div className={styles.contactMessege}>
+              <label className={styles.contact_label}>Mensaje</label>
+              <textarea
+                id="w3review"
+                name="w3review"
+                rows="5"
+                className={styles.contact_textarea}
+              />
+            </div>
+            <div className={styles.contact_buttonContainer}>
+              <Button>Enviar</Button>
+            </div>
+          </form>
+          <article className={styles.contactInfo}>
+            <div className={styles.contactInfo_section}>
+              <h3 className={styles.contact_label}>Ubicación</h3>
+              <small>Buenos Aires, Argentina</small>
+            </div>
+            <div className={styles.contactInfo_section}>
+              <h3 className={styles.contact_label}>E-mail</h3>
+              <small>sebastian@alotama.com</small>
+            </div>
+          </article>
+        </section>
       </article>
-    </section>
+    </Layout>
   )
 }
 
